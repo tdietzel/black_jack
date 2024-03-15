f0d1f3c6fbbce54275d2d57d85b8865c1c5964bd
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2024-03-15 10:20:25 -0700

    add: complete test #4 print_hand

--- src/app.py
+++ src/app.py
@@ -1,27 +1,51 @@
 import random
 
-class black_jack:
+class BlackJack:
+  def __init__(self):
+    pass
+
+  @staticmethod
   def create_deck():
     ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
     suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
-    deck = [{'rank': rank, 'suit': suit} for suit in suits for rank in ranks]
+    deck = [{ 'rank': rank, 'suit': suit } for suit in suits for rank in ranks]
     random.shuffle(deck)
     return deck
 
+  @staticmethod
   def card_value(card):
     rank = card['rank']
-    if rank in [ 'J', 'Q', 'K']:
+    if rank in ['J', 'Q', 'K']:
       return 10
     elif rank == 'A':
       return 11
     else:
       return int(rank)
 
+  @staticmethod
   def deal_hand(deck):
     player_hand = [deck.pop(), deck.pop()]
     house_hand = [deck.pop(), deck.pop()]
     return player_hand, house_hand
 
+  @staticmethod
+  def print_hand(hand, hidden):
+    if hidden:
+      print('House card:', f"{ hand[0]['rank'] } of { hand[0]['suit'] }", '\nHouse card: ~ Flipped Over ~')
+    else:
+      for card in hand:
+        print('Your cards:', f"{ card['rank'] } of { card['suit'] }")
+    return hidden #changed to hidden from hand for testing
+
+  @staticmethod
   def play_game():
-    deck = black_jack.create_deck()
-    return black_jack.deal_hand(deck)
+    blackjack = BlackJack()
+    deck = blackjack.create_deck() #creates deck
+    player_hand, house_hand = blackjack.deal_hand(deck) #deals hand
+
+    blackjack.print_hand(player_hand, False)
+    blackjack.print_hand(house_hand, True)
+
+    return 
+
+BlackJack.play_game()
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,26 +1,35 @@
 import sys
 sys.path.append("../src")
-from app import black_jack
+from app import BlackJack
 import unittest
 
-class test_black_jack(unittest.TestCase):
+class BlackJackTests(unittest.TestCase):
 
   def test_create_deck(self):
-    deck = black_jack.create_deck()
+    deck = BlackJack.create_deck()
 
     self.assertEqual(len(deck), 52)
 
   def test_card_value(self):
-    deck = black_jack.create_deck()
-    cardValue = black_jack.card_value(deck[0])
+    deck = BlackJack.create_deck()
+    cardValue = BlackJack.card_value(deck[0])
 
     self.assertIn(cardValue, range(11))
 
   def test_deal_hand(self):
-    playerHand, houseHand = black_jack.play_game()
+    deck = BlackJack.create_deck()
+    playerHand, houseHand = BlackJack.deal_hand(deck)
+
     self.assertEqual(len(playerHand), 2)
     self.assertEqual(len(houseHand), 2)
 
+  def test_print_hand(self):
+    deck = BlackJack.create_deck()
+    playerHand, houseHand = BlackJack.deal_hand(deck)
+
+    self.assertEqual(BlackJack.print_hand(playerHand, hidden = False), False)
+    self.assertEqual(BlackJack.print_hand(houseHand, hidden = True), True)
+    
 if __name__ == '__main__':
   unittest.main()
 
