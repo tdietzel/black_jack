cadcea7fd46f8d15682fcb413a9d68720157a64d
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2024-03-15 09:28:19 -0700

    add: complete test #3 deal_hand

--- src/app.py
+++ src/app.py
@@ -1,17 +1,27 @@
 import random
 
-def create_deck():
-  ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-  suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
-  deck = [{'rank': rank, 'suit': suit} for suit in suits for rank in ranks]
-  random.shuffle(deck)
-  return deck
+class black_jack:
+  def create_deck():
+    ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+    deck = [{'rank': rank, 'suit': suit} for suit in suits for rank in ranks]
+    random.shuffle(deck)
+    return deck
 
-def card_value(card):
-  rank = card['rank']
-  if rank in [ 'J', 'Q', 'K']:
-    return 10
-  elif rank == 'A':
-    return 11
-  else:
-    return int(rank)
+  def card_value(card):
+    rank = card['rank']
+    if rank in [ 'J', 'Q', 'K']:
+      return 10
+    elif rank == 'A':
+      return 11
+    else:
+      return int(rank)
+
+  def deal_hand(deck):
+    player_hand = [deck.pop(), deck.pop()]
+    house_hand = [deck.pop(), deck.pop()]
+    return player_hand, house_hand
+
+  def play_game():
+    deck = black_jack.create_deck()
+    return black_jack.deal_hand(deck)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,20 +1,27 @@
 import sys
 sys.path.append("../src")
-import app
+from app import black_jack
 import unittest
 
 class test_black_jack(unittest.TestCase):
 
   def test_create_deck(self):
-    deck = app.create_deck()
+    deck = black_jack.create_deck()
 
     self.assertEqual(len(deck), 52)
 
   def test_card_value(self):
-    deck = app.create_deck()
-    cardValue = app.card_value(deck[0])
+    deck = black_jack.create_deck()
+    cardValue = black_jack.card_value(deck[0])
 
     self.assertIn(cardValue, range(11))
 
+  def test_deal_hand(self):
+    playerHand, houseHand = black_jack.play_game()
+    self.assertEqual(len(playerHand), 2)
+    self.assertEqual(len(houseHand), 2)
+
 if __name__ == '__main__':
   unittest.main()
+
+#play_game is untested line #25
